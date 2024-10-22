0e515f05011b4d828e5fda4ae4bde54cc712691e
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-22 17:14:45 -0400

    reached original function code

--- main.ts
+++ main.ts
@@ -5,22 +5,30 @@
  * This program compares 2 generated numbers
 */
 
+let randomNumber1: number = randint(0, 99)
+let randomNumber2: number = randint(0, 99)
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-let number1: number = 0
-let number2: number = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(randomNumber1)
+    basic.showIcon(IconNames.Happy)
+});
 
-//
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showNumber(randomNumber2)
+    basic.showIcon(IconNames.Happy)
+});
 
-input.buttonIsPressed(Button.A)
-basic.clearScreen()
-basic.showNumber(number1)
-basic.showIcon(IconNames.Happy)
-
-
-
-
-
-
-
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    if (randomNumber1 > randomNumber2) {
+        basic.showString("#1 > #2")
+    } else if (randomNumber1 < randomNumber2) {
+        basic.showString("#1 < #2")
+    }
+    basic.showIcon(IconNames.Sad)
+})
